b5d7221538e8a1610b86e815d25725468c1ebbff
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-16 07:48:14 -0300

    termina "[M1S11] Ex 2 - Configuração do tema utilizando styled components"

---
+++ src/themes/themeDark.js
@@ -0,0 +1,8 @@
+export const themeDark = {
+  colors: {
+    backgroundPrimary: "#341c00",
+    backgroundSecondary: "#192140",
+    primary: "#ffcb47",
+    secondary: "#5373e7",
+  },
+};
---
+++ src/themes/themeLight.js
@@ -0,0 +1,8 @@
+export const themeLight = {
+  colors: {
+    backgroundPrimary: "#fff0ee",
+    backgroundSecondary: "#fff1e7",
+    primary: "#db4324",
+    secondary: "#a07653",
+  },
+};
